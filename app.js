41bf8a1131dae647e00e6e80677849029564ddbd
Kenpham1506 <165395073+Kenpham1506@users.noreply.github.com>
2026-01-08 06:46:23 +0700

    Update app.js

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const engine = new webllm.MLCEngine();
 // Load a fast, small model
 output.textContent = "Downloading model (first time only)...";
 
-await engine.reload("TinyLlama-1.1B-Chat-v1.0");
+await engine.reload("phi-2-q4f16_1");
 
 output.textContent = "Ready.";
 
