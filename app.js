7baae90b3604417e4fb2312da4236a81ba36eac0
Kenpham1506 <165395073+Kenpham1506@users.noreply.github.com>
2026-01-15 13:29:58 +0700

    Update app.js

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ sendBtn.onclick = async () => {
 
   const res = await engine.chat.completions.create({
     messages: [{ role: "user", content: prompt }],
-    max_tokens: 12048,
+    max_tokens: 92048,
     temperature: 0.7,
   });
 
